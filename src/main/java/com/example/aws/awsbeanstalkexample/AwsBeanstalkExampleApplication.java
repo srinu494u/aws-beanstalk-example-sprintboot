44d44014a6e7bf31f3c8a59e76eeeb9bf1ab6262
srinivasarao.gunda <srinu@gmail.com>
2020-02-16 17:22:07 +0530

    beanstalk project

---
+++ src/main/java/com/example/aws/awsbeanstalkexample/AwsBeanstalkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws.awsbeanstalkexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBeanstalkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBeanstalkExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aws/awsbeanstalkexample/AwsBeanstalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws.awsbeanstalkexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBeanstalkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
